645c51b6985605e6ba405449100f738a5297f052
Cicciolina Miranda <cicci.miranda@gmail.com>
2018-10-27 15:40:53 +0800

    work on using path

--- app/src/main/java/com/zdominguez/sdksandbox/DrawShapes2Activity.java
+++ app/src/main/java/com/zdominguez/sdksandbox/DrawShapes2Activity.java
@@ -1,5 +1,6 @@
 package com.zdominguez.sdksandbox;
 
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,11 +26,37 @@ public class DrawShapes2Activity extends AppCompatActivity implements DrawingVie
 
         relativeLayout = (RelativeLayout) findViewById(R.id.container);
 
+        final Path path1 = new Path();
+
+        path1.moveTo(0,0);
+        path1.lineTo(50, 0);
+        path1.lineTo(50 + 120, 50);
+        path1.lineTo(120, 50);
+        path1.lineTo(0, 0);
+        final Path path2 = new Path();
+        path2.moveTo(10,10);
+        path2.lineTo(50, 0);
+        path2.lineTo(50 + 120, 50);
+        path2.lineTo(120, 50);
+        path2.lineTo(10, 10);
+        final Path path3 = new Path();
+        path3.moveTo(50,50);
+        path3.lineTo(50, 0);
+        path3.lineTo(50 + 120, 50);
+        path3.lineTo(120, 50);
+        path3.lineTo(50, 50);
+        final Path path4 = new Path();
+        path4.moveTo(60,60);
+        path4.lineTo(50, 0);
+        path4.lineTo(50 + 120, 50);
+        path4.lineTo(120, 50);
+        path4.lineTo(60, 60);
+
         List<RectanglePoints> rectanglePointsList = new ArrayList<RectanglePoints>() {{
-            add(new RectanglePoints(new Rect(130, 130, 180, 180), ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_red_dark), "Red"));
-            add(new RectanglePoints(new Rect(133, 160, 177, 177), ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_blue_dark), "Blue"));
-            add(new RectanglePoints(new Rect(133, 133, 177, 160), ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_orange_dark), "Orange"));
-            add(new RectanglePoints(new Rect(200, 200, 300, 300), ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_green_dark), "Green"));
+            add(new RectanglePoints(path1, ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_red_dark), "Red"));
+            add(new RectanglePoints(path2, ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_blue_dark), "Blue"));
+            add(new RectanglePoints(path3, ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_orange_dark), "Orange"));
+            add(new RectanglePoints(path4, ContextCompat.getColor(DrawShapes2Activity.this, android.R.color.holo_green_dark), "Green"));
 
         }};
 
